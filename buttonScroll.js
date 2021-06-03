d76bf9ebb1cbf2dfd2e33481a8617f2762d97f13
kavishvarshney <kavishkumarvars@gmail.com>
2021-06-03 22:23:20 +0530

    Initial commit

---
+++ buttonScroll.js
@@ -0,0 +1,19 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When to show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
